edb436968154307bc3eccda2bc33541f5072b48a
TonyInBeijing <yuehaowei1126@gmail.com>
2022-01-05 22:50:14 +0800

    页面布局

--- src/components/home/footer.vue
+++ src/components/home/footer.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="footer padding-xl bg-black">这是footer slot</div>
+    <div class="footer padding-xl bg-black text-center">
+        <a href="https://beian.miit.gov.cn/">京ICP备2021019610号-1</a>
+    </div>
 </template>
 <script setup lang="ts"></script>
 <style scoped>
-
 </style>
--- src/components/home/main.vue
+++ src/components/home/main.vue
@@ -19,23 +19,27 @@ function MyIntroduction() {
  *
  * @description 一些爱好 (致贫原因)
  * ps5 & switch 双机党，买游戏的快乐大于玩游戏的快乐
- * Audi A5 3.0TFSI quattro B8.5 APR StageII 车主
+ * Audi A5 B8.5 SportBack 3.0TFSI quattro APR StageII 车主
  * 
  * @description 关于这个网站
  * 这不是一个纯技术博客，内容会比较丰富多彩一些
  * 前期内容会更多的放在 gitbook 上，争取一年内更新 100 篇各类文章
  * 会逐步完善网站功能，争取早日让它看起来像一个博客
  * 
+ * 
+ * Think twice, Code Once
  */
             </pre>
         </div>
-        <div class="padding-xl">
+        <!-- <div class="padding-xl">
             <el-row>
-                <el-col :span="8">1</el-col>
+                <el-col :span="8">
+                    <el-button size="large" color="#000000">GitHub</el-button>
+                </el-col>
                 <el-col :span="8">2</el-col>
                 <el-col :span="8">3</el-col>
             </el-row>
-        </div>
+        </div> -->
     </div>
 </template>
 <script setup lang="ts"></script>
@@ -44,6 +48,6 @@ function MyIntroduction() {
     min-height: 94vh;
 }
 .slogan-container {
-    height: 84vh;
+    height: 94vh;
 }
 </style>
